658283a3db55bd2e8f6ec7894b11ed879da2e35e
Nandan Manjunatha <Nannigalaxy@gmail.com>
2025-04-27 21:42:55 +0530

    Replace assert with if condition

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -163,7 +163,8 @@ def readColmapSceneInfo(path, images, eval, llffhold=8):
             xyz, rgb, _ = read_points3D_binary(bin_path)
         except:
             xyz, rgb, _ = read_points3D_text(txt_path)
-        assert xyz and rgb, f"Error loading {path} data. Point data must exist in either points3D.bin or points3D.txt"
+        if (xyz.shape[0] == 0) and (rgb.shape[0] == 0):
+            raise ValueError(f"Error loading {path} data. Point data must exist in either points3D.bin or points3D.txt")
         storePly(ply_path, xyz, rgb)
     try:
         pcd = fetchPly(ply_path)
